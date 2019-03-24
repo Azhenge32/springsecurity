bfa2e2a9bd3bc108f97a15926b7cddbbf2546c90
Azhenge32 <az2960426127@gmail.com>
2019-03-24 14:37:09 +0800

    Initial commit

---
+++ spring-security-browser/src/main/java/com.azhen/MyUserDetailService.java
@@ -0,0 +1,28 @@
+package com.azhen;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class MyUserDetailService implements UserDetailsService {
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        String password = passwordEncoder.encode("123456");
+        log.info("数据库密码是: " + password);
+        String passwordOld = "$2a$10$d64eRlpHD3j7GPbX5iXnuuGHA0oCDlH12vk.ju5whMkG8bxYtrQP.";
+        return new User(username, passwordOld,
+                true, true, true, true,
+                AuthorityUtils.commaSeparatedStringToAuthorityList("admin"));
+    }
+}
