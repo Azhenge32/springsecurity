637716777ce74faad92cc76bd43a53988118cd46
Azhenge32 <az2960426127@gmail.com>
2019-03-28 17:54:55 +0800

    用户名密码登陆返回token

--- spring-security-app/src/main/java/com.azhen.app/ResourcesServerConfig.java
+++ spring-security-app/src/main/java/com.azhen.app/ResourcesServerConfig.java
@@ -1,9 +1,42 @@
 package com.azhen.app;
 
+import com.azhen.core.authentication.AbstractChannelSecurityConfig;
+import com.azhen.core.authentication.mobile.SmsCodeAuthenticationSecurityConfig;
+import com.azhen.core.properties.SecurityConstants;
+import com.azhen.core.properties.SecurityProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @Configuration
 @EnableResourceServer
-public class ResourcesServerConfig {
+public class ResourcesServerConfig extends AbstractChannelSecurityConfig {
+    @Autowired
+    private SecurityProperties securityProperties;
+    @Autowired
+    private SmsCodeAuthenticationSecurityConfig smsCodeAuthenticationSecurityConfig;
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        applyPasswordAuthenticationConfig(http);
+        http
+            .apply(smsCodeAuthenticationSecurityConfig)
+            .and()
+            .authorizeRequests()
+            .antMatchers(
+                    SecurityConstants.DEFAULT_UNAUTHENTICATION_URL,
+                    SecurityConstants.DEFAULT_LOGIN_PROCESSING_URL_MOBILE,
+                    securityProperties.getBrowser().getLoginPage(),
+                    SecurityConstants.DEFAULT_VALIDATE_CODE_URL_PREFIX+"/*",
+                    securityProperties.getBrowser().getSignUpUrl(),
+                    securityProperties.getBrowser().getSession().getSessionInvalidUrl()+".json",
+                    securityProperties.getBrowser().getSession().getSessionInvalidUrl()+".html",
+                    securityProperties.getBrowser().getLogoutUrl(),
+                    "/user/regist", "/leave")
+            .permitAll()
+            .anyRequest()
+            .authenticated()
+            .and()
+            .csrf().disable();
+    }
 }
--- spring-security-app/src/main/java/com.azhen.app/authentication/MyAuthenticationSuccessHandler.java
+++ spring-security-app/src/main/java/com.azhen.app/authentication/MyAuthenticationSuccessHandler.java
@@ -7,15 +7,26 @@ import com.azhen.core.properties.LoginType;
 import com.azhen.core.properties.SecurityProperties;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections.MapUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.codec.Base64;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.common.exceptions.UnapprovedClientAuthenticationException;
+import org.springframework.security.oauth2.provider.*;
+import org.springframework.security.oauth2.provider.token.AbstractTokenGranter;
+import org.springframework.security.oauth2.provider.token.AuthorizationServerTokenServices;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.ServletWebRequest;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @Component("imoocAuthenticationSuccessHandler")
@@ -27,19 +38,62 @@ public class MyAuthenticationSuccessHandler extends SavedRequestAwareAuthenticat
 	@Autowired
 	private SecurityProperties securityProperties;
 
+	@Autowired
+	private ClientDetailsService clientDetailsService;
+
+	@Autowired
+	private AuthorizationServerTokenServices tokenServices;
+
+
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
-			Authentication authentication) throws IOException, ServletException {
+			Authentication authentication) throws IOException {
+		String header = request.getHeader("Authorization");
 
-		log.info("登录成功");
-
-		if (LoginType.JSON.equals(securityProperties.getBrowser().getLoginType())) {
-			response.setContentType("application/json;charset=UTF-8");
-			response.getWriter().write(objectMapper.writeValueAsString(authentication));
-		} else {
-			super.onAuthenticationSuccess(request, response, authentication);
+		if (header == null || !header.startsWith("Basic ")) {
+			throw new UnapprovedClientAuthenticationException("请求头中无client信息");
 		}
 
+		String[] tokens = extractAndDecodeHeader(header, request);
+		assert tokens.length == 2;
+		String clientId = tokens[0];
+		ClientDetails clientDetails = clientDetailsService.loadClientByClientId(clientId);
+		Map<String, String[]> parameterMap = request.getParameterMap();
+		Map<String, String> parameters = new HashMap<>(parameterMap.size());
+		for (Map.Entry<String, String[]> entry : parameterMap.entrySet()) {
+			parameters.put(entry.getKey(), entry.getValue()[0]);
+		}
+
+		TokenRequest tokenRequest = new TokenRequest(MapUtils.EMPTY_MAP, clientId, clientDetails.getScope(), "custom");
+		OAuth2Request oAuth2Request = tokenRequest.createOAuth2Request(clientDetails);
+		OAuth2Authentication auth2Authentication =  new OAuth2Authentication(oAuth2Request, authentication);
+		OAuth2AccessToken accessToken = tokenServices.createAccessToken(auth2Authentication);
+		response.setHeader("Cache-Control", "no-store");
+		response.setHeader("Pragma", "no-cache");
+		response.setContentType("application/json;charset=UTF-8");
+		response.getWriter().write(objectMapper.writeValueAsString(accessToken));
 	}
 
+	private String[] extractAndDecodeHeader(String header, HttpServletRequest request)
+			throws IOException {
+
+		byte[] base64Token = header.substring(6).getBytes("UTF-8");
+		byte[] decoded;
+		try {
+			decoded = Base64.decode(base64Token);
+		}
+		catch (IllegalArgumentException e) {
+			throw new BadCredentialsException(
+					"Failed to decode basic authentication token");
+		}
+
+		String token = new String(decoded, "UTF-8");
+
+		int delim = token.indexOf(":");
+
+		if (delim == -1) {
+			throw new BadCredentialsException("Invalid basic authentication token");
+		}
+		return new String[] { token.substring(0, delim), token.substring(delim + 1) };
+	}
 }
